088c6b2bd517d8183326218171c29f65fc9510d6
ok <ok@gee.ks.ua>
2016-04-12 00:24:33 +0300

    hit.results = nil

--- sunspot/lib/sunspot/search/field_group.rb
+++ sunspot/lib/sunspot/search/field_group.rb
@@ -50,7 +50,7 @@ module Sunspot
           hits_for_class = id_hit_hash[class_name]
           data_accessor.load_all(ids).each do |result|
             hits = hits_for_class.delete(Adapters::InstanceAdapter.adapt(result).id.to_s)
-            hits.each{ |hit| hit.result = result }
+            hits.each{ |hit| hit.result = result rescue Rails.logger.error('error1') }
           end
           hits_for_class.values.each { |hits| hits.each{|hit| hit.result = nil rescue Rails.logger.error('error1') } }
         end
